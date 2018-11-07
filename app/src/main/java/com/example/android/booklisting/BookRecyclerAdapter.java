240a7abf679c4697a5bb25c398ad0bce73d41662
Osama <osama.afzal97@gmail.com>
2018-11-08 00:31:07 +0500

    1.Implemented 3 methods of RecyclerView Adapter 2.Added the attributes title and author for testing

--- app/src/main/java/com/example/android/booklisting/BookRecyclerAdapter.java
+++ app/src/main/java/com/example/android/booklisting/BookRecyclerAdapter.java
@@ -1,36 +1,71 @@
 package com.example.android.booklisting;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapter.MyViewHolder>{
 
+    private ArrayList<Book> mBooks;
+
+    public BookRecyclerAdapter(ArrayList<Book> mBooks) {
+        this.mBooks = mBooks;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View bookView = inflater.inflate(R.layout.list_item_view_2, viewGroup,false);
+
+        MyViewHolder viewHolder = new MyViewHolder(bookView);
+
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
 
+        Book book = mBooks.get(position);
+
+        TextView book_title_1 = myViewHolder.book_title_1;
+        book_title_1.setText(book.getBookTitle());
+
+        TextView book_author_1 = myViewHolder.book_author_1;
+        book_author_1.setText(book.getBookAuthor());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mBooks.size();
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder{
 
+        TextView book_title_1;
+        TextView book_author_1;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            book_title_1 =(TextView) itemView.findViewById(R.id.book_title_1);
+            book_author_1 = (TextView) itemView.findViewById(R.id.book_author_1);
         }
+
+
+
     }
+
 
 }
